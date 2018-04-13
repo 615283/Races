f35a11fd144c909f7cc8ff246c190bb555e1f1c8
James <615283@users.noreply.github.com>
2018-04-13 19:00:07 +0100

    Classes and stub methods for Cancel/Confirm Commands

---
+++ src/main/java/com/georlegacy/general/races/commands/CancelCommand.java
@@ -0,0 +1,14 @@
+package com.georlegacy.general.races.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class CancelCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        return false;
+    }
+
+}
---
+++ src/main/java/com/georlegacy/general/races/commands/ConfirmCommand.java
@@ -0,0 +1,13 @@
+package com.georlegacy.general.races.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class ConfirmCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        return false;
+    }
+}
