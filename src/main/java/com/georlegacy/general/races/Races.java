4091a0faea4c10d05fc3efbc05072bc2c3d14daf
James <615283@users.noreply.github.com>
2018-04-13 15:20:28 +0100

    Registered Commands in basic form

--- src/main/java/com/georlegacy/general/races/Races.java
+++ src/main/java/com/georlegacy/general/races/Races.java
@@ -2,8 +2,10 @@
 
 package com.georlegacy.general.races;
 
+import com.georlegacy.general.races.commands.SetupCommand;
 import com.sk89q.worldedit.bukkit.WorldEditPlugin;
 import org.bukkit.Bukkit;
+import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -13,11 +15,19 @@ public class Races extends JavaPlugin {
     public void onEnable() {
         PluginManager pm = getServer().getPluginManager();
 
+        registerCommands();
     }
 
     @Override
     public void onDisable() {
 
+    }
+
+    private void registerCommands() {
+        PluginCommand races = getCommand("races");
+
+        PluginCommand racesSetup = getCommand("racessetup");
+        racesSetup.setExecutor(new SetupCommand());
     }
 
     public static WorldEditPlugin getWorldEdit() {
